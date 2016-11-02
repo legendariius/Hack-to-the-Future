8070110e8babb05fdc973f715e26472fbc28b141
legendariius <louis.cheinisse@yahoo.fr>
2016-11-02 16:12:45 +0100

    Avancée mineure (rien de permanent)

--- index.php
+++ index.php
@@ -7,6 +7,28 @@
         <link href="style.css" rel="stylesheet">
     </head>
     <body>
-
+        <div class="maincontainer">
+            <div class="col-lg-12">
+                <img src="images/logo/HTTF.jpg" alt="Hack to the Future" class="logo">
+            </div>
+            <div class="row">
+                <div class="topbanner">
+                    <h1>BANNIERE A FINIR</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="navbar-nav nav">
+                        <ul class="navbar">
+                            <li class="active"><a href="index.php">Accueil</a></li>
+                            <li><a href="incription.php">Inscription</a></li>
+                            <li><a href="partenaires.php">Partenaires</a></li>
+                            <li><a href="asso.php">Qui sommes-nous?</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
