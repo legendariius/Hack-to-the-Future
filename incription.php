fd92290a61b0c5c339a3f37285a64ba418ba1c9b
legendariius <louis.cheinisse@yahoo.fr>
2016-11-02 10:19:40 +0100

    Ajout des fichiers de base du projet

---
+++ incription.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Louis
+ * Date: 02/11/2016
+ * Time: 09:17
+ */
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Accueil</title>
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+
+    </body>
+</html>
